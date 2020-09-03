f94864c832978ffaa5a5c85b3d633d031343d1a6
Jan Minařík <jan.minarik@scalesoft.cz>
2020-09-03 14:26:52 +0200

    Added purpose of use options

--- Solution/cz.kr_vysocina.nis.v11.core/Models/PurposeOfUse.cs
+++ Solution/cz.kr_vysocina.nis.v11.core/Models/PurposeOfUse.cs
@@ -7,5 +7,11 @@ namespace cz.kr_vysocina.nis.v11.core.Models
 
         [System.Runtime.Serialization.EnumMember(Value = @"TREATMENT")]
         TREATMENT = 1,
+        
+        [System.Runtime.Serialization.EnumMember(Value = @"PATIENT")]
+        PATIENT = 2,
+        
+        [System.Runtime.Serialization.EnumMember(Value = @"NONNCP")]
+        NONNCP = 3,
     }
 }
